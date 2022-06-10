1a36cb49612ec56407c0a23a8f9671d588844e19
Alberto Ronchi <lestatdelioncurt@libero.it>
2022-06-10 10:08:39 +0200

    Additional config for react-navigation (react-screens)

--- android/app/src/main/java/com/checkmyrepo/MainActivity.java
+++ android/app/src/main/java/com/checkmyrepo/MainActivity.java
@@ -4,6 +4,10 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 
+// for react-native-screens
+import android.os.Bundle;
+
+
 public class MainActivity extends ReactActivity {
 
   /**
@@ -22,6 +26,12 @@ public class MainActivity extends ReactActivity {
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
     return new MainActivityDelegate(this, getMainComponentName());
+  }
+
+  // for react-native-screens
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 
   public static class MainActivityDelegate extends ReactActivityDelegate {
