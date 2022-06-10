7bd2ed21afe788685eaf25f06a07a2eb10bcf6a7
Alberto Ronchi <lestatdelioncurt@libero.it>
2022-06-10 17:06:35 +0200

    Styling navigator header

--- App.jsx
+++ App.jsx
@@ -5,14 +5,22 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './components/home';
 import DataEntry from './components/data-entry';
 
+const backImage = require('./assets/img/back.png');
+
 const Stack = createNativeStackNavigator();
 
-const getOptions = ({ route }) => ({ title: route.params.name });
+const getOptions = ({ route }) => ({
+  title:                 route.params.name,
+  headerBackImageSource: backImage
+});
+
+const noHeader  = { headerShown: false };
+const noSHhadow = { headerShadowVisible: false };
 
 const WelcomeScreen = () => (
   <NavigationContainer>
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={Home} />
+    <Stack.Navigator screenOptions={noSHhadow}>
+      <Stack.Screen name="Home" component={Home} options={noHeader} />
       <Stack.Screen name="DataEntry" component={DataEntry} options={getOptions} />
     </Stack.Navigator>
   </NavigationContainer>
