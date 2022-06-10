824301046c3991f42fe365adfd83579d9119da0e
Alberto Ronchi <lestatdelioncurt@libero.it>
2022-06-10 11:04:08 +0200

    Added theme file

---
+++ style/theme.js
@@ -0,0 +1,16 @@
+export const COLORS = {
+  WHITE:      '#FFFFFF',
+  BLACK:      '#000000',
+  LIGHT_GREY: '#989898'
+};
+
+export const FONT_SIZES = {
+  BIG:    30,
+  MEDIUM: 20
+};
+
+export const FONT_TYPES = {
+  REGULAR: 'OpenSans-Regular',
+  BOLD:    'OpenSans-Bold',
+  ITALIC:  'OpenSans-Italic'
+};
